0b980e8c1c39705554eef5fff09e24c8cc1b3cb9
Jebi_ <jeongighyeon576@gmail.com>
2025-05-17 21:39:39 +0900

    Finish Glitch Effect for Button(Maybe)
    Im god

--- Assets/File_Ikhyun/Scripts/GlitchEffecter.cs
+++ Assets/File_Ikhyun/Scripts/GlitchEffecter.cs
@@ -1,87 +1,58 @@
+using System;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 
 public class GlitchEffecter : MonoBehaviour
 {
-    [SerializeField] private Material glitchMaterial;
-    
-    [SerializeField] private float maxIntensity = 0.5f;
-    [SerializeField] private float minIntensity = 0.1f;
-    
-    [SerializeField] private float maxTimer = 2f;
-    [SerializeField] private float minTimer = 0.2f;
-    [SerializeField] private float decayDuration = 3f;
+    [SerializeField] private TextMeshProUGUI tmpText;
+    [SerializeField] private float noiseScale = 2f;
 
-    private static GlitchEffecter instance;
-    private Dictionary<Graphic, Coroutine> activeEffects = new Dictionary<Graphic, Coroutine>();
+    private Material tmpMaterial;
 
-    private void Awake()
-    {
-        instance = this;
+    private void Awake() {
+        SetNoiseScale(0);
+        tmpMaterial = tmpText.fontMaterial;
     }
 
-    
-    public static void Active(Graphic target)
-    {
-        if (instance == null || target == null) return;
+    private void Update() {
+        tmpMaterial.SetFloat("_NoiseScale", noiseScale);
+    }
 
-        
-        if (instance.activeEffects.ContainsKey(target))
+    public void SetNoiseScale(float newScale)
+    {
+        noiseScale = newScale;
+
+        if (tmpMaterial != null)
         {
-            instance.StopCoroutine(instance.activeEffects[target]);
+            tmpMaterial.SetFloat("_NoiseScale", noiseScale);
         }
+    }
+    
+    public void StopSelectEffect()
+    {
+        SetNoiseScale(0);
+    }
 
-        
-        Material instanceMaterial = new Material(instance.glitchMaterial);
-        target.material = instanceMaterial;
+    public void PlayHoverEffect()
+    {
+        if (!isActiveAndEnabled) return;
 
-        
-        Coroutine coroutine = instance.StartCoroutine(instance.LerpEffect(target, instanceMaterial));
-        instance.activeEffects[target] = coroutine;
+        SetNoiseScale(2);
+    }
+
+    // public void PlayClickEffect()
+    // {
+    //     if (!isActiveAndEnabled) return;
+    //
+    //     StopAllCoroutines();
+    //     SetTextMaterial(glitchMaterial);
+    // }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        StopSelectEffect();
     }
 
     
-    public static void Deactivate(Graphic target)
-    {
-        if (instance == null || target == null) return;
-
-        if (instance.activeEffects.ContainsKey(target))
-        {
-            instance.StopCoroutine(instance.activeEffects[target]);
-            instance.activeEffects.Remove(target);
-        }
-
-        
-        target.material = null;
-    }
-
-    
-    private IEnumerator LerpEffect(Graphic target, Material material)
-    {
-        float elapsedTime = 0f;
-
-        // 시작 값 설정
-        material.SetFloat("_Intensity", maxIntensity);
-        material.SetFloat("_Timer", maxTimer);
-
-        while (elapsedTime < decayDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            
-            float t = elapsedTime / decayDuration;
-            
-            float intensity = Mathf.Lerp(maxIntensity, minIntensity, Mathf.SmoothStep(0f, 1f, t));
-            float timer = Mathf.Lerp(maxTimer, minTimer, Mathf.SmoothStep(0f, 1f, t));
-
-            material.SetFloat("_Intensity", intensity);
-            material.SetFloat("_Timer", timer);
-
-            yield return null;
-        }
-        
-        material.SetFloat("_Intensity", minIntensity);
-        material.SetFloat("_Timer", minTimer);
-    }
 }
