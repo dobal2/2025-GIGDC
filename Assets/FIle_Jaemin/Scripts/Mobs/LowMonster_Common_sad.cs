badd662af0cc9e98d0a091a408e2b1bb34121c94
dobal2 <poopgamesgoggle@gmail.com>
2025-06-25 21:30:14 +0900

    sad fixed

--- Assets/FIle_Jaemin/Scripts/Mobs/LowMonster_Common_sad.cs
+++ Assets/FIle_Jaemin/Scripts/Mobs/LowMonster_Common_sad.cs
@@ -7,6 +7,8 @@ public class LowMonster_Common_sad : Monster
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private GameObject firePos;
+    
+    private bool isAttacking = false;
 
     protected override void Start()
     {
@@ -37,17 +39,52 @@ public class LowMonster_Common_sad : Monster
     }
     protected override void Attack()
     {
+        if (isAttacking)
+            return;
+        isAttacking = true;
         anim.SetTrigger("Attack");
-        StartCoroutine(Fire());
         
         StartCoroutine(WaitToAttack(attackCoolDown));    
+        attackCoroutine = StartCoroutine(AttackRoutine());
+    }
+
+    public override void TakeDamage(float amount)
+    {
+        hp -= amount;
+
+        // 공격 중이라면 끊기
+        if (attackCoroutine != null)
+        {
+            StopCoroutine(attackCoroutine);
+            attackCoroutine = null;
+            isAttacking = false;
+
+            anim.ResetTrigger("Attack");
+            anim.Play("Idle");
+        }
+
+        // 경직 시작
+        if (stunCoroutine != null) StopCoroutine(stunCoroutine);
+        stunCoroutine = StartCoroutine(DoStun(0.5f)); // 예: 0.5초 경직
+
+        TakeDamageAnimation();
+        //KnockBack(knockBackDir);
+
+        if (hp <= 0) Die();
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        yield return new WaitForSeconds(attackCoolDown);
+        attackCoroutine = null;
+        isAttacking = false;
     }
     
+    
+    
 
-    IEnumerator Fire()
+    public void Fire()
     {
-        yield return new WaitForSeconds(1.4f);
-        
         Debug.Log("Sad Fire");
         Vector2 dir = (player.position - transform.position).normalized;
         GameObject projectile = Instantiate(projectilePrefab, firePos.transform.position, Quaternion.identity);
