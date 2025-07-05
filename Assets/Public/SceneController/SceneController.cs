c2d77791d5f4f0262fca18d0f908b78d5b5b4b83
seoil0404 <seoil6795@gmail.com>
2025-07-05 22:27:36 +0900

    Stage2 Added

--- Assets/Public/SceneController/SceneController.cs
+++ Assets/Public/SceneController/SceneController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static SceneController;
@@ -15,6 +18,15 @@ public class SceneController : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+    }
+
+    [InitializeOnLoadMethod]
+    private static void InitializeSceneLink()
+    {
+        sceneLinks ??=
+            ((SceneType[])Enum.GetValues(typeof(SceneType)))
+            .Select(stage => new SceneLink(stage, stage.ToString()))
+            .ToArray();
     }
 
     public void LoadScene(SceneType sceneType)
@@ -47,16 +59,7 @@ public class SceneController : MonoBehaviour
         throw new System.ArgumentOutOfRangeException();
     }
 
-    private static SceneLink[] sceneLinks =
-    {
-        new SceneLink(SceneType.Stage1_1, "Stage1_1"),
-        new SceneLink(SceneType.Stage1_2, "Stage1_2"),
-        new SceneLink(SceneType.Stage1_3, "Stage1_3"),
-        new SceneLink(SceneType.Stage1_4, "Stage1_4"),
-        new SceneLink(SceneType.Stage1_5, "Stage1_5"),
-        new SceneLink(SceneType.Stage1_6, "Stage1_6"),
-        new SceneLink(SceneType.Stage1_7, "Stage1_7")
-    };
+    private static SceneLink[] sceneLinks;
 
     private class SceneLink
     {
@@ -78,6 +81,46 @@ public class SceneController : MonoBehaviour
         Stage1_4, 
         Stage1_5, 
         Stage1_6, 
-        Stage1_7
+        Stage1_7,
+        Stage1_8,
+        Stage1_9,
+        Stage1_10,
+        Stage1_11,
+        Stage2_1,
+        Stage2_2,
+        Stage2_3,
+        Stage2_4,
+        Stage2_5,
+        Stage2_6,
+        Stage2_7,
+        Stage2_8,
+        Stage2_9,
+        Stage2_10,
+        Stage3_1,
+        Stage3_2,
+        Stage3_3,
+        Stage3_4,
+        Stage3_5,
+        Stage3_6,
+        Stage3_7,
+        Stage3_8,
+        Stage3_9,
+        Stage3_10,
+        Stage3_11,
+        Stage3_12,
+        Stage3_13,
+        Stage3_14,
+        Stage3_15,
+        Stage4_1,
+        Stage4_2,
+        Stage4_3,
+        Stage4_4,
+        Stage4_5,
+        Stage4_6,
+        Stage4_7,
+        Stage4_8,
+        Stage4_9,
+        Stage4_10,
+        Stage4_11,
     }
 }
