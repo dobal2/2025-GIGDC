008ea13de0033f4c287b75279ee2c89e04181f8d
seoil0404 <seoil6795@gmail.com>
2025-07-11 23:14:20 +0900

    PlayerHealth Fixed

--- Assets/Public/SceneController/RuntimeSceneControllerGenerator.cs
+++ Assets/Public/SceneController/RuntimeSceneControllerGenerator.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public static class RuntimeSceneControllerGenerator
 {
+#if UNITY_EDITOR
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void OnGameStart()
     {
         GameObject sceneControllerObject = new GameObject(typeof(SceneController).Name);
         sceneControllerObject.AddComponent<SceneController>();
     }
+#endif
 }
