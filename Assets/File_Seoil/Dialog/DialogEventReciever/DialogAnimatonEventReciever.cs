b30ced7cc1959b1d31cb5c4e2da7167f065e7ea2
seoil0404 <seoil6795@gmail.com>
2026-02-10 20:50:03 +0900

    Update ~Stage4_1

--- Assets/File_Seoil/Dialog/DialogEventReciever/DialogAnimatonEventReciever.cs
+++ Assets/File_Seoil/Dialog/DialogEventReciever/DialogAnimatonEventReciever.cs
@@ -14,7 +14,11 @@ public class DialogAnimatonEventReciever : DialogEventRecieverBase
 
     private void RegisterAnimations()
     {
-
+        animationDictionaries.Add("Captain_Walk", Captain_Walk);
+        animationDictionaries.Add("Captain_Sitdown", Captain_Sitdown);
+        animationDictionaries.Add("Captain_Transform", Captain_Transform);
+        animationDictionaries.Add("Death_Walk", Death_Walk);
+        animationDictionaries.Add("Death_Greet", Death_Greet);
     }
 
     private void AnimateCharacter(string line)
@@ -24,5 +28,35 @@ public class DialogAnimatonEventReciever : DialogEventRecieverBase
             animationDictionaries[line].Invoke();
         }
         else throw new KeyNotFoundException(line);
+    }
+
+    private void Captain_Walk()
+    {
+        var captainController = FindAnyObjectByType<CaptainController>();
+        captainController.Captain_Walk();
+    }
+
+    private void Captain_Sitdown()
+    {
+        var captainController = FindAnyObjectByType<CaptainController>();
+        captainController.Captain_Sitdown();
+    }
+
+    private void Captain_Transform()
+    {
+        var captainController = FindAnyObjectByType<CaptainController>();
+        captainController.Captain_Transform();
+    }
+
+    private void Death_Walk()
+    {
+        var captainController = FindAnyObjectByType<CaptainController>();
+        captainController.Death_Walk();
+    }
+
+    private void Death_Greet()
+    {
+        var captainController = FindAnyObjectByType<CaptainController>();
+        captainController.Death_Greet();
     }
 }
